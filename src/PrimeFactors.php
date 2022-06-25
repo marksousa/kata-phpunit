dd589eed33314119cee6dfe6187a6f966d3aa585
marksousa <sousa.marcosrk@gmail.com>
2022-06-25 13:25:08 -0300

    Clean Code

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -8,19 +8,15 @@ class PrimeFactors
     {
 
         $factors = [];
-        $divisor = 2;
 
         // 1. O numero é divisivel por 2?
         // 2. Se true, divida por 2. Se falso, vá para o próximo candidato e tente novamente.
         // 3. Repita
 
-        while($number > 1){
-            while($number % $divisor === 0){
+        for ($divisor=2; $number > 1; $divisor++) {
+            for (; $number % $divisor === 0 ; $number /= $divisor) { 
                 $factors[] = $divisor;
-    
-                $number = $number / $divisor;
             }
-            $divisor ++;
         }
 
         return $factors;
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -13,7 +13,7 @@ class PrimeFactorsTest extends TestCase
      * @test 
      * @dataProvider factors
      * */
-    public function it_generates_prime_factors_for($number, $expected)
+    public function it_generates_prime_factors_for_a_number($number, $expected)
     {
         $factors = new PrimeFactors();
 
@@ -31,6 +31,7 @@ class PrimeFactorsTest extends TestCase
             [6, [2, 3]],
             [8, [2, 2, 2]],
             [100, [2, 2, 5, 5]],
+            [999, [3, 3, 3, 37]],
         ];
     }
     
