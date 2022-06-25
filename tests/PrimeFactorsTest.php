9d0542e39296a015fe7493bb67b218b9f1572ac8
marksousa <sousa.marcosrk@gmail.com>
2022-06-25 13:16:07 -0300

    Refatorando o Teste - less hardcoding

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -9,38 +9,30 @@ use App\PrimeFactors;
  */
 class PrimeFactorsTest extends TestCase
 {
-    /** @test */
-    public function it_generates_prime_factors_for_1()
+    /** 
+     * @test 
+     * @dataProvider factors
+     * */
+    public function it_generates_prime_factors_for($number, $expected)
     {
         $factors = new PrimeFactors();
 
-        $this->assertEquals([], $factors->generate(1));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_2()
-    {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_3()
-    {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_4()
-    {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([2, 2], $factors->generate(4));
+        $this->assertEquals($expected, $factors->generate($number));
     }
     
+    public function factors()
+    {
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2, 2]],
+            [5, [5]],
+            [6, [2, 3]],
+            [8, [2, 2, 2]],
+            [100, [2, 2, 5, 5]],
+        ];
+    }
     
 
 }
